808734ff1d19de2fb8da897fe9dd21d99ed6ecc4
João Carlos <Joo Carlos@JCDA>
2019-08-22 20:02:22 -0300

    Primeiro commit

---
+++ TortasDoces/public_html/opiniao.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+$nome = $_GET['nome'];
+$telefone= $_GET['telefone'];
+$email = $_GET['email'];
+$recado= $_GET['comentario'];
+
+
+
+echo $nome."</br>";
+
+echo $email."</br>";
+
+
+
+
+?>
